4ee5af6141e24c9e906b91a454d7aca355ea869f
chrispoturalski <chrispotural@gmail.com>
2023-03-08 08:51:22 -0800

    added about section

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <h2 className='text-4xl font-bold inline border-b-4'>About</h2>
+            </div>
+
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui laboriosam alias consequuntur? Vero autem, sunt adipisci numquam officia quod ea provident quisquam minima repudiandae, cupiditate, iusto sequi ipsum aperiam rerum?
+            </p>
+
+            <br />
+
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla nisi, accusamus neque numquam officia odio est quos laudantium earum sequi iste, exercitationem, aspernatur esse assumenda magni perferendis dolorum perspiciatis impedit?
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
